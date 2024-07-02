3636bc760048ccaacffca2905f21ee4ab36df8d5
nameless <130059555+MeErickss@users.noreply.github.com>
2024-07-02 20:17:05 -0300

    API GITHUB

--- src/script.js
+++ src/script.js
@@ -1,4 +1,26 @@
+const repositors = document.querySelector('[corpo]')
+
+function getApiGithUB(user){
+    fetch(`https://api.github.com/users/${user}/repos`).then(
+        async res => {
+            if( !res.ok){
+                throw new Error(res.status)
+            }
+            let data = await res.json()
+            data.map(item => {
+                let project = document.createElement('div')
+                
+                project.innerHTML = `${item.name}`
+
+                repositors.appendChild(project)
+            })
+            console.log(data)
+        })
+}
+
+
 const inpt = document.querySelector("[inpt]")
+const card = document.querySelector("[card]")
 const btn = document.querySelector("[btn]")
 const pfp = document.querySelector("[pfp]")
 const nameU = document.querySelector("[name]")
@@ -8,5 +30,6 @@ const description = document.querySelector("[description]")
 const language = document.querySelector("[language]")
 
 btn.addEventListener("click", function(){
-    login.innerText = "@" + inpt.value 
+    card.style = "display: block;"
+    getApiGithUB(inpt.value)
 })
