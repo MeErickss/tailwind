3e849386330b9b6c1d96326819eef639eb656ed9
nameless <130059555+MeErickss@users.noreply.github.com>
2024-07-03 22:03:39 -0300

    landscape aleatorio

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
 const card = document.querySelector('[card]')
 
+const imgs = [
+    "https://img.freepik.com/fotos-gratis/uma-pintura-de-um-lago-de-montanha-com-uma-montanha-ao-fundo_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1719792000&semt=ais_user", "https://img.freepik.com/fotos-gratis/pico-da-montanha-nevada-sob-a-majestade-generativa-da-galaxia-estrelada-ai_188544-9650.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1719964800&semt=ais_user",
+    "https://i.pinimg.com/736x/60/63/da/6063da107454d50c1d7786cc2c2bac75.jpg",
+    "https://i.pinimg.com/564x/be/8c/c9/be8cc91ca24dea985ec394dfd8e5ff74.jpg",
+    "https://i.pinimg.com/736x/42/91/ef/4291ef9d28ddbb65e1d8ac2a2a632642.jpg",
+    "https://i.pinimg.com/564x/33/40/f5/3340f59c8b98a834b01b66f927599131.jpg",
+    "https://i.pinimg.com/564x/81/2c/20/812c206e207d0e671075183f3683e7ef.jpg",
+    "https://i.pinimg.com/564x/eb/e7/a9/ebe7a9c46af8fa3b0531c30f7b953e1a.jpg",
+    "https://i.pinimg.com/564x/6a/94/3f/6a943fcdf9944bfca4da1f5a38aaa60b.jpg",
+    "https://i.pinimg.com/564x/3c/a0/18/3ca01809f2dee927870f01e2659f4f50.jpg"
+]
+
 
 function getApiGithUB(nameU){
     fetch(`https://api.github.com/users/${nameU}`).then(
@@ -9,9 +21,12 @@ function getApiGithUB(nameU){
             }
 
             let data = await res.json()
-            let user = document.createElement('div')
 
-            user.innerHTML = `<article class="flex flex-wrap justify-center text-center">
+            perfil.innerHTML = `<header>
+                <div class="head">
+                    <img class="rounded h-64 w-full" src="${imgs[Math.floor(Math.random() * (imgs.length))]}" alt="background">
+                </div>
+            </header><article class="flex flex-wrap justify-center text-center">
                     <img pfp class="rounded-full absolute -mt-24 border-8 border-white" width="150" src="${data.avatar_url}" alt="profile picture">
                     <div class="py-14 h-16">
                         <p name class="text-2xl">${data.name}</p>
@@ -24,7 +39,6 @@ function getApiGithUB(nameU){
                 <p repositorios><strong>REPOSITÓRIOS</strong></p>
             </nav>`
 
-                card.appendChild(user)
             card.style = "display: block;"
             getRepos(nameU)
         })
@@ -43,21 +57,19 @@ function getRepos(user){
             for (let i = 0; i<data.length; i++){
                 let project = document.createElement('div')
 
-                project.innerHTML += `<section class="px-5 my-4 h-24 rounded">
+                project.innerHTML = `<section class="px-5 my-4 h-24 rounded">
                     <strong nameR class="nameR">${(data[i].name)}</strong>
                     <p description class="description">${data[i].description}</p>
                     <span language class="bg-zinc-400 rounded px-4"><strong>#${data[i].language}</strong></span>
                 </section>`
                 repos.appendChild(project)
             }
-
-            console.log(data)
-
         })
 }
 
 const inpt = document.querySelector("[inpt]")
 const btn = document.querySelector("[btn]")
+const perfil = document.querySelector("[perfil]")
 
 btn.addEventListener("click", function(){
     getApiGithUB(inpt.value)
